842203167d0c3a97b6eb04ea5632e1cea1f5932a
Pandiyan <pandiec@gmail.com>
2022-07-06 19:31:54 +0530

    for_each-with-az-instancetype-check

--- for_each-with-az-instancetype-check/ec2instance.tf
+++ for_each-with-az-instancetype-check/ec2instance.tf
@@ -1,18 +1,10 @@
 # EC2 Instance
 resource "aws_instance" "myec2vm" {
-<<<<<<< HEAD
-  ami                    = data.aws_ami.amzlinux2.id
-  instance_type          = var.instance_type
-  user_data              = file("${path.module}/app1-install.sh")
-  key_name               = var.instance_keypair
-  vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
-=======
   ami = data.aws_ami.amzlinux.id
   instance_type = var.instance_type
   user_data = file("${path.module}/app1-install.sh")
   key_name = var.instance_keypair
   vpc_security_group_ids = [ aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id   ]
->>>>>>> 67fdf119d711db054c70ee6bf94614dc906bb31c
   # Create EC2 Instance in all Availabilty Zones of a VPC  
   #for_each = toset(data.aws_availability_zones.my_azones.names)
   for_each = toset(keys({ for az, details in data.aws_ec2_instance_type_offerings.my_ins_type :
