f02af6c1ebb6cba5350faa95d8bbdcdf1b982361
Pandiyan <pandiec@gmail.com>
2022-07-11 13:56:57 +0530

    EBS-Kubernetes-SampleApp-Terraform

--- EBS-Kubernetes-SampleApp-Terraform/terraform-manifests-UMS-WebApp/c1-versions.tf
+++ EBS-Kubernetes-SampleApp-Terraform/terraform-manifests-UMS-WebApp/c1-versions.tf
@@ -13,7 +13,7 @@ terraform {
   }
   # Adding Backend as S3 for Remote State Storage
   backend "s3" {
-    bucket = "terraform-on-aws-eks"
+    bucket = "terraform-pandiyan"
     key    = "dev/ebs-sampleapp-demo/terraform.tfstate"
     region = "us-east-1" 
 
