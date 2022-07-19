ec398e330b61684ccf407cb365f36ae24a71b227
Pandiyan <pandiec@gmail.com>
2022-07-19 18:32:20 +0530

    EKS-Monitoring-Logging-Terraform

---
+++ EKS-Monitoring-Logging-Terraform/02-cloudwatchagent-fluentbit-terraform-manifests/c1-versions.tf
@@ -0,0 +1,31 @@
+# Terraform Settings Block
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.14"
+     }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "~> 2.11"
+    }    
+    http = {
+      source = "hashicorp/http"
+      version = "~> 2.1"
+    }     
+    kubectl = {
+      source = "gavinbunney/kubectl"
+      version = "1.14.0"
+    }     
+  }
+  # Adding Backend as S3 for Remote State Storage
+  backend "s3" {
+    bucket = "terraform-pandiyan"
+    key    = "dev/eks-cloudwatch-agent/terraform.tfstate"
+    region = "us-east-1" 
+
+    # For State Locking
+    dynamodb_table = "dev-eks-cloudwatch-agent"    
+  }     
+}
